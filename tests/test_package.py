2fc11bcb437e0fe68c72e919bd9ea4d7692ce966
La5er <larinserg@gmail.com>
2026-08-09 12:42:12 +0700

    chore: stage TestPyPI rc2

--- tests/test_package.py
+++ tests/test_package.py
@@ -3,7 +3,7 @@ from telegram_bot_factory.cli import main
 
 
 def test_package_version() -> None:
-    assert __version__ == "0.1.0"
+    assert __version__ == "0.1.0rc2"
 
 
 def test_cli_help(capsys: object) -> None:
